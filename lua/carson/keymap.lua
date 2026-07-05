463fcdf779c9daa3341529628e6b4c9f41a53cd1
carson <carson@nerdware.ca>
2026-07-05 11:53:21 -0700

    more gooder

--- lua/carson/keymap.lua
+++ lua/carson/keymap.lua
@@ -13,4 +13,11 @@ vim.keymap.set({ "n", "x" }, "<leader>p", '"+p', { desc = "Paste from system cli
 -- Toggle comment on the visual selection (uses built-in `gc` + commentstring).
 vim.keymap.set("x", "<leader>//", "gc", { remap = true, desc = "Toggle comment on selection" })
 vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'View line diagnostics' })
-
+vim.keymap.set('n', '<leader>ch', function() 
+	vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+	vim.notify(string.format("Inlay Hint Enabled? %s", vim.lsp.inlay_hint.is_enabled({ bufnr = 0 })))
+ end )
+vim.keymap.set('n', '<leader>cl', function() 
+	vim.lsp.codelens.enable(not vim.lsp.codelens.is_enabled()) 
+	vim.notify(string.format("Code Lens Enabled? %s", vim.lsp.codelens.is_enabled({ bufnr = 0 })))
+ end )
--- lua/lang/init.lua
+++ lua/lang/init.lua
@@ -5,9 +5,9 @@ vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(args)
 		local client = vim.lsp.get_client_by_id(args.data.client_id)
 		if client:supports_method("textDoucment/completion") then
-			vim.lsp.completion.enable(true, client.id, args.buf, {
-				autotrigger = true,
-			})
+			local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+			client.server_capabilities.completionProvider.triggerCharacters = chars
+			vim.lsp.completion.enable(true, client.id, args.buf, {autotrigger = true})
 		end
 	end,
 })
